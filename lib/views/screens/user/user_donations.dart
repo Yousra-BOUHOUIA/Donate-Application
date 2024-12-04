81d9af66b9ed2acc3c376e74b9049dbde071af69
manarbentayeb <146331154+manarbentayeb@users.noreply.github.com>
2024-12-05 00:24:57 +0100

    Create user_donations.dart

---
+++ lib/views/screens/user/user_donations.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:donate_application/views/widgets/user_card.dart';
+import 'package:donate_application/themes/colors.dart';
+import 'package:donate_application/views/widgets/footer.dart'; 
+class DonationsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appBackgroundColor,
+        title: const Text("Donations"),
+        centerTitle: true, // Center the title in the AppBar
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: appBackgroundColor,
+      body: SingleChildScrollView(
+        // Ensure the entire body is scrollable vertically
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Event 1
+              createCard(
+                "Clothing Drive",
+                "Join us for a clothing drive to provide winter clothes to those in ...",
+                'assets/images/donation_image.webp',
+                150,
+                250,
+              ),
+              // Event 2
+              createCard(
+                "Food Donation Campaign",
+                "We are collecting non-perishable food items to help families... ",
+                'assets/images/event_image.webp',
+                180,
+                280,
+              ),
+              // Event 3
+              createCard(
+                "Book Donation Initiative",
+                "Help us donate books to schools in underprivileged areas...",
+                'assets/images/helping_image.jpg',
+                160,
+                270,
+              ),
+              // Event 4
+              createCard(
+                "Toy Drive for Children",
+                "This holiday season, let's bring joy to children by donating toys. Every donation matters!",
+                'assets/images/volenteering_image.webp',
+                140,
+                230,
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const Footer(), // Added Footer widget here
+    );
+  }
+}

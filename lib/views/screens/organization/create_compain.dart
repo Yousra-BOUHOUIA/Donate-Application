6045e038554440659f6e90eda520f93ecf7aa442
Lydiabenhamouche <147343945+Lydiabenhamouche@users.noreply.github.com>
2024-12-04 09:46:51 +0100

    Add files via upload

---
+++ lib/views/screens/organization/create_compain.dart
@@ -0,0 +1,210 @@
+// in pubspec.yaml add image_picker: ^0.8.7+3 in dependencies 
+// correct the overflow of the type text field 
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'build3DTextField.dart';
+import 'build3DDropdown.dart';
+import 'colors.dart';
+
+class CreateCampaignScreen extends StatefulWidget {
+  @override
+  _CreateCampaignScreenState createState() => _CreateCampaignScreenState();
+}
+
+class _CreateCampaignScreenState extends State<CreateCampaignScreen> {
+  final TextEditingController _dueDateController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _resourcesController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  File? _selectedImage;
+  String? _selectedType;
+
+  final List<String> campaignTypes = [
+    "Fundraiser",
+    "Volunteer Recruitment",
+    "Awareness Campaign",
+    "Donation Drive",
+    "Charity Event"
+  ];
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  String? _validateDate(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Please enter a due date";
+    }
+    final dateRegex = RegExp(r'^\d{4}-\d{2}-\d{2}$'); // Format: YYYY-MM-DD
+    if (!dateRegex.hasMatch(value)) {
+      return "Invalid date format (YYYY-MM-DD)";
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        elevation: 0,
+        title: const Text(
+          'Create Campaign',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {},
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            GestureDetector(
+              onTap: _pickImage,
+              child: Container(
+                height: 200,
+                width: 80,
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: _selectedImage != null
+                    ? ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.file(
+                          _selectedImage!,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.image, size: 50, color: Colors.grey),
+                          SizedBox(height: 10),
+                          Text(
+                            'Upload pictures',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+              ),
+            ),
+            const SizedBox(height: 25),
+            Row(
+              children: [
+                Expanded(
+                  child: build3DDropdown(
+                    'Type',
+                    campaignTypes,
+                    (value) {
+                      setState(() {
+                        _selectedType = value!;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: build3DTextField(
+                    'Due Date',
+                    controller: _dueDateController,
+                    validator: _validateDate,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 25),
+            build3DTextField(
+              'Title',
+              controller: _titleController,
+            ),
+            const SizedBox(height: 25),
+            build3DTextField(
+              'Location',
+              controller: _locationController,
+            ),
+            const SizedBox(height: 25),
+            build3DTextField(
+              'Resources Needed',
+              controller: _resourcesController,
+            ),
+            const SizedBox(height: 25),
+            build3DTextField(
+              'Description',
+              controller: _descriptionController,
+            ),
+            const SizedBox(height: 35),
+            Center(
+              child: SizedBox(
+                width: 150,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: appButtonColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(35),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_dueDateController.text.isEmpty ||
+                        _selectedType == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Please complete the form'),
+                        ),
+                      );
+                      return;
+                    }
+
+                    // Reset the form
+                    _dueDateController.clear();
+                    _titleController.clear();
+                    _locationController.clear();
+                    _resourcesController.clear();
+                    _descriptionController.clear();
+                    setState(() {
+                      _selectedImage = null;
+                      _selectedType = null;
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Campaign Created!'),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    'Create',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/organization/home_org.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:project/colors.dart';
+import 'org_card.dart';
+import 'event_card.dart';
+
+
+class OrgHomePage extends StatelessWidget {
+  const OrgHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            // Sidebar logic
+          },
+          icon: const Icon(Icons.menu),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const Text("Tataou3"),
+            IconButton(
+              onPressed: () {
+                // Logo action (optional)
+              },
+              icon: const Icon(Icons.volunteer_activism),
+            ),
+          ],
+        ),
+        backgroundColor: appBackgroundColor,
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Events:",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              SizedBox(
+                height: cardHeight,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    eventCard('Event 1 Description', 'lib/assets/images/event_picture.jpg', '10'),
+                    const SizedBox(width: 16),
+                    eventCard('Event 2 Description', 'lib/assets/images/event_picture.jpg', '20'),
+                    const SizedBox(width: 16),
+                    eventCard('Event 3 Description', 'lib/assets/images/event_picture.jpg', '30'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                "Categories:",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: const [
+                    Chip(
+                      label: Text("All", style: TextStyle(color: Colors.white)),
+                      backgroundColor: appButtonColor,
+                      shape: StadiumBorder(),
+                    ),
+                    SizedBox(width: chipSpacing),
+                    Chip(
+                      label: Text("Donations", style: TextStyle(color: Colors.white)),
+                      backgroundColor: appButtonColor,
+                      shape: StadiumBorder(),
+                    ),
+                    SizedBox(width: chipSpacing),
+                    Chip(
+                      label: Text("Events", style: TextStyle(color: Colors.white)),
+                      backgroundColor: appButtonColor,
+                      shape: StadiumBorder(),
+                    ),
+                    SizedBox(width: chipSpacing),
+                    Chip(
+                      label: Text("Get Donation", style: TextStyle(color: Colors.white)),
+                      backgroundColor: appButtonColor,
+                      shape: StadiumBorder(),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                "Latest Campaigns:",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              createCard(
+                "Day of Compassion",
+                "On the Day of Compassion, we aim to bring smiles to the children in orphanages. Join us to make a difference.",
+                'lib/assets/images/org_image.jpg',
+                120,
+                200,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add post logic
+        },
+        backgroundColor: appButtonColor,
+        child: const Icon(Icons.add, color: Colors.white),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}

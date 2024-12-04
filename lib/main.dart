802b32a513dd7a38df87a004b7ee37aa509208ce
HibaAyadi <145927742+HibaAyadi@users.noreply.github.com>
2024-12-04 04:14:43 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:donate_application/views/screens/user/user_edit_profile.dart';
 import 'package:donate_application/views/screens/user/user_post.dart';
 import 'package:donate_application/views/screens/user/user_profile_details.dart';
+import 'package:donate_application/views/screens/organization/org_notifications.dart';
+import 'package:donate_application/views/screens/organization/user_donations.dart';
+import 'package:donate_application/views/screens/common/donation_description.dart';
+import 'package:donate_application/views/screens/common/event_description.dart';
+import 'package:donate_application/views/screens/user/user_notification.dart';
 
 
 import 'package:donate_application/views/screens/organization/org_edit_profile.dart';
@@ -24,7 +29,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: orgProfileDetailsScreen(),
+      home: NotificationsPage(),
+      routes: {
+  
+        NotificationsPage.pageRoute: (ctx) => const NotificationsPage(),
+        UsersDonationsScreen.pageRoute: (ctx) =>  UsersDonationsScreen (),  
+        DonationDescriptionScreen.pageRoute: (ctx) =>  DonationDescriptionScreen (),  
+        EventDescriptionScreen.pageRoute: (ctx) =>   EventDescriptionScreen (),  
+        UserNotification.pageRoute: (ctx) =>    UserNotification(),  
+        
+         
+        
+},
     );
   }
 }

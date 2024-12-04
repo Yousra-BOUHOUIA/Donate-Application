2c571184fc78d6363ca4a2a9b362b4ef1467957c
manarbentayeb <146331154+manarbentayeb@users.noreply.github.com>
2024-12-05 00:11:50 +0100

    Update colors.dart

--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,9 +1,33 @@
-
 import 'package:flutter/material.dart';
 
-const Color appBackgroundColor = Color(0xFFF8F6F6); 
-const Color appBarColor = Color(0xFFF8F6F6);  
+const Color appBackgroundColor = Color(0xFFF8F6F6);
+const Color appBarColor = Color(0xFFF8F6F6);
 const Color appButtonColor = Color(0xFF0E1B48);
 const Color footerbtncolor = Color(0xFFE2CAD8);
-const Color percentIndicator =  Color(0xFF87A7D0);
+const Color percentIndicator = Color(0xFF87A7D0);
 const Color Description_Text = Color(0xFF666D80);
+
+
+// Gradient colors for the forms
+const Color formGradientStart = Color.fromRGBO(193, 141, 180, 1);
+const Color formGradientEnd = Color.fromRGBO(135, 167, 208, 1);
+
+// Gradient colors for the top pages
+const Color topGradientStart = Color(0xFF0E1B48);
+const Color topGradientEnd = Color(0xFF87A7D0);
+// arrow and top bar colors
+const Color topBarColor = Color.fromARGB(255, 255, 255, 255);
+
+// Main colors used in the app
+const Color primaryColor = Color(0xFF0E1B48);
+const Color backgroundColor = Color.fromARGB(255, 233, 222, 233);
+
+// Text colors
+const Color textColor = Colors.white;
+const Color labelColor = Color(0xFF0E1B48);
+const Color buttonTextColor = Colors.white;
+
+// Button Colors
+const Color buttonColor = Color(0xFF0E1B48);
+
+

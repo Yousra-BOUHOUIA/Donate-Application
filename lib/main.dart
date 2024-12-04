0812b620a1284b172236f16aef8e36f3e4df1672
HibaAyadi <145927742+HibaAyadi@users.noreply.github.com>
2024-12-05 00:21:50 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ import '/views/screens/user/user_edit_profile.dart';
 import '/views/screens/user/user_post.dart';
 import '/views/screens/user/user_profile_details.dart';
 import '/views/screens/organization/org_notifications.dart';
-import '/views/screens/organization/user_donations.dart';
-import '/views/screens/common/donation_description.dart';
-import '/views/screens/common/event_description.dart';
+import '/views/screens/organization/users_donations.dart';
+
+import 'package:donate_application/views/screens/organization/org_donation_description.dart';
+import 'package:donate_application/views/screens/organization/org_event_description.dart';
+import 'package:donate_application/views/screens/user/user_donation_description.dart';
+import 'package:donate_application/views/screens/user/user_event_description.dart';
+
+
 import '/views/screens/user/user_notification.dart';
 
 
@@ -32,11 +37,14 @@ class MyApp extends StatelessWidget {
 
       routes: {
   
-        NotificationsPage.pageRoute: (ctx) => const NotificationsPage(),
+        OrgNotification.pageRoute: (ctx) => const OrgNotification(),
+        UserNotification.pageRoute: (ctx) => const UserNotification(),
         UsersDonationsScreen.pageRoute: (ctx) =>  UsersDonationsScreen (),  
-        DonationDescriptionScreen.pageRoute: (ctx) =>  DonationDescriptionScreen (),  
-        EventDescriptionScreen.pageRoute: (ctx) =>   EventDescriptionScreen (),  
-        UserNotification.pageRoute: (ctx) =>    UserNotification(),          
+        
+        UserDonationDescriptionScreen.pageRoute:(ctx) => const UserDonationDescriptionScreen(),
+        UserEventDescriptionScreen.pageRoute:(ctx) => const UserEventDescriptionScreen(),
+        OrgDonationDescriptionScreen.pageRoute:(ctx) => const OrgDonationDescriptionScreen(),
+        OrgEventDescriptionScreen.pageRoute:(ctx) => const OrgEventDescriptionScreen(),         
       },
 
 

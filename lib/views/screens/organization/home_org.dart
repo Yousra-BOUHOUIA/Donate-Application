d25491a190f02b0dcd767bb8f14d71d979e60b27
HibaAyadi <145927742+HibaAyadi@users.noreply.github.com>
2024-12-05 16:37:07 +0100

    Add files via upload

--- lib/views/screens/organization/home_org.dart
+++ lib/views/screens/organization/home_org.dart
@@ -64,11 +64,11 @@ class OrgHomePage extends StatelessWidget {
                 child: ListView(
                   scrollDirection: Axis.horizontal,
                   children: [
-                    eventCard(context,'Event 1 Description', 'assets/images/event_picture.jpg', '10'),
+                    eventCard(context,true,'Event 1 Description', 'assets/images/event_picture.jpg', '10'),
                     const SizedBox(width: 16),
-                    eventCard(context,'Event 2 Description', 'assets/images/event_picture.jpg', '20'),
+                    eventCard(context,true,'Event 2 Description', 'assets/images/event_picture.jpg', '20'),
                     const SizedBox(width: 16),
-                    eventCard(context,'Event 3 Description', 'assets/images/event_picture.jpg', '30'),
+                    eventCard(context,true,'Event 3 Description', 'assets/images/event_picture.jpg', '30'),
                   ],
                 ),
               ),
@@ -147,7 +147,7 @@ class OrgHomePage extends StatelessWidget {
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 8),
-              createCard(context,
+              createCard(context,false,
                 "Day of Compassion",
                 "On the Day of Compassion, we aim to bring smiles to the children in orphanages. Join us to make a difference.",
                 'assets/images/org_image.jpg',

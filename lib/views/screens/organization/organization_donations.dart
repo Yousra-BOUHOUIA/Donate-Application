5dd0a16328cee78039dc702a7315f2e9aa1858f0
HibaAyadi <145927742+HibaAyadi@users.noreply.github.com>
2024-12-05 15:43:58 +0100

    Update organization_donations.dart

--- lib/views/screens/organization/organization_donations.dart
+++ lib/views/screens/organization/organization_donations.dart
@@ -5,7 +5,7 @@ import 'package:donate_application/views/widgets/footer.dart';
 
 class DonationsPage extends StatelessWidget {
   const DonationsPage({super.key});
-  static const pageRoute = '/org_donations';
+  static const String pageRoute = '/org_donations';
 
   @override
   Widget build(BuildContext context) {
@@ -65,7 +65,7 @@ class DonationsPage extends StatelessWidget {
           ),
         ),
       ),
-    bottomNavigationBar: const Footer(),
+    bottomNavigationBar: const Footer(isOrganization: true,),
     );
 
   }

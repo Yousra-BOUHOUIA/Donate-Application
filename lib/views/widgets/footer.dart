7d2196ac413049f1db7030a10129f45fa225b911
HibaAyadi <145927742+HibaAyadi@users.noreply.github.com>
2024-12-06 17:15:46 +0100

    Update footer.dart

--- lib/views/widgets/footer.dart
+++ lib/views/widgets/footer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:donate_application/themes/colors.dart';
+
 
 import '/imports/organization_barrel.dart';
 import '/imports/user_barrel.dart';
@@ -9,7 +9,7 @@ import '/imports/user_barrel.dart';
 class Footer extends StatefulWidget {
   final bool isOrganization;
 
-  const Footer({Key? key, required this.isOrganization}) : super(key: key);
+  const Footer({super.key, required this.isOrganization});
 
   @override
   State<Footer> createState() => _FooterState();
@@ -18,53 +18,9 @@ class Footer extends StatefulWidget {
 class _FooterState extends State<Footer> {
   int? _selectedIndex; // Keeps track of the selected tab index (null means no tab selected)
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _updateSelectedIndex();
-  }
 
-  void _updateSelectedIndex() {
-    final ModalRoute? currentRoute = ModalRoute.of(context);
-    if (currentRoute != null) {
-      final newIndex = _getIndexFromRoute(currentRoute.settings.name);
-      if (newIndex != _selectedIndex) {
-        setState(() {
-          _selectedIndex = newIndex;
-        });
-      }
-    }
-  }
+  
 
-  int? _getIndexFromRoute(String? route) {
-    if (widget.isOrganization) {
-      switch (route) {
-        case OrgHomePage.pageRoute:
-          return 0;
-        case DonationsPage.pageRoute:
-          return 1;
-        case OrgNotification.pageRoute:
-          return 2;
-        case OrgProfilePage.pageRoute:
-          return 3;
-        default:
-          return null; // No tab selected for undefined routes
-      }
-    } else {
-      switch (route) {
-        case UserHomePage.pageRoute:
-          return 0;
-        case UserDonations.pageRoute:
-          return 1;
-        case UserNotification.pageRoute:
-          return 2;
-        case UserProfilePage.pageRoute:
-          return 3;
-        default:
-          return null; // No tab selected for undefined routes
-      }
-    }
-  }
 
   void _onTabChange(int index) {
     setState(() {
@@ -83,7 +39,7 @@ class _FooterState extends State<Footer> {
           Navigator.pushNamed(context, OrgNotification.pageRoute);
           break;
         case 3:
-          Navigator.pushNamed(context, OrgProfilePage.pageRoute);
+          Navigator.pushNamed(context, OrgProfileDetailsScreen.pageRoute);
           break;
       }
     } else {
@@ -98,7 +54,7 @@ class _FooterState extends State<Footer> {
           Navigator.pushNamed(context, UserNotification.pageRoute);
           break;
         case 3:
-          Navigator.pushNamed(context, UserProfilePage.pageRoute);
+          Navigator.pushNamed(context, UserProfileDetailsScreen.pageRoute);
           break;
       }
     }
@@ -114,7 +70,7 @@ class _FooterState extends State<Footer> {
           backgroundColor: Colors.white,
           color: Colors.black,
           activeColor: Colors.black,
-          tabBackgroundColor: footerbtncolor,
+          
           padding: const EdgeInsets.all(16),
           gap: 8,
           selectedIndex: _selectedIndex ?? -1, // Highlights the active tab or none if null
@@ -122,19 +78,19 @@ class _FooterState extends State<Footer> {
           tabs: const [
             GButton(
               icon: Icons.home,
-              text: 'Home',
+              
             ),
             GButton(
               icon: Icons.volunteer_activism,
-              text: 'Donations',
+             
             ),
             GButton(
               icon: Icons.notifications,
-              text: 'Notifications',
+              
             ),
             GButton(
               icon: Icons.account_box,
-              text: 'Profile',
+              
             ),
           ],
         ),

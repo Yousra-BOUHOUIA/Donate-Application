d9b83fb2617a22d2546246a7fb2b27cbced1d166
Yousra-BOUHOUIA <121288486+Yousra-BOUHOUIA@users.noreply.github.com>
2024-12-18 00:19:57 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\yousr\\AppData\\Local\\devflutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\yousr\\OneDrive\\Bureau\\Mobile dev\\donate_application" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\yousr\\AppData\\Local\\devflutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\yousr\\OneDrive\\Bureau\\Mobile dev\\donate_application"
+  "FLUTTER_ROOT=C:\\Users\\yousr\\AppData\\Local\\devflutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\yousr\\OneDrive\\Bureau\\Mobile dev\\donate_application\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\yousr\\OneDrive\\Bureau\\Mobile dev\\donate_application"
+  "FLUTTER_TARGET=C:\\Users\\yousr\\OneDrive\\Bureau\\Mobile dev\\donate_application\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\yousr\\OneDrive\\Bureau\\Mobile dev\\donate_application\\.dart_tool\\package_config.json"
+)
